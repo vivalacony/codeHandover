7e887f8151f529298e6b9f8eda48655ba6083a9a
vivalacony <cofallon@tcd.ie>
2015-03-31 11:57:58 +0100

    Update databaseFunctions.py

--- database/databaseFunctions.py
+++ database/databaseFunctions.py
@@ -14,6 +14,7 @@ import titleDatabase
 import statsDatabase
 import keywordDatabase
 import time
+import MySQLdb
 
 
 def addNewProject(secretUrl, Uname, title, Author, SourceCode, Description, Input, Output, 
@@ -33,6 +34,14 @@ def addNewProject(secretUrl, Uname, title, Author, SourceCode, Description, Inpu
 
 	#TODO: put keyword database stuff here
 	return projectId
+	
+	#db=MySQLdb.connect(host="localhost",user="root",passwd="woot",db="TestDB")
+  
+	#cur = db.cursor()
+	#cur.execute("SELECT UserID FROM CNGL_User_Tbl WHERE UserName LIKE '" + Uname + "'")
+	#AddedByID=cur.fetchone()
+  
+	#cur.execute("INSERT INTO CNGL_Webservices_Tbl (Name,Description,SourceCodeLink,Author,AddedByID,ExpectedInput,Output) VALUES ('" + title + "', '" + Description + "','" + SourceCode + "', '" + "','" + Author + "'," + AddedByID +",'" + Input +"','" + Output + "'")
 
 def getSearch(keywordlist):
 	result = []
@@ -81,6 +90,12 @@ def removeProject(projectId):
 	titleDatabase.removeTitle(projectInfo['Uname'])
 	projectDatabase.removeProject(projectId)
 	globalDatabase.removeProjectIdFromProjectList(projectId)
+	
+	#db=MySQLdb.connect(host="localhost",user="root",passwd="woot",db="TestDB")
+  
+	#cur = db.cursor()
+	
+	#cur.execute("DELETE FROM CNGL_Webservices_Tbl WHERE WebserviceID = " + projectId)
 
 
 #function should be run whenever redis database is cleared
@@ -101,18 +116,41 @@ def changeUsername(userId, newUsername):
 	usernameDatabase.removeUsername( userInfo['username'] )
 	userDatabase.changeUsername(userId, newUsername)
 	usernameDatabase.addUsername(newUsername, userId)
+	
+	#db=MySQLdb.connect(host="localhost",user="root",passwd="woot",db="TestDB")
+  
+	#cur = db.cursor()
+	#cur.execute("UPDATE CNGL_User_Tbl SET UserName = '" + newUsername + "' WHERE UserID = " + userId)
+	
 
 def changePasswordHash(userId, newPasswordHash):
 	userDatabase.changePasswordHash(userId, newPasswordHash)
+	
+	#db=MySQLdb.connect(host="localhost",user="root",passwd="woot",db="TestDB")
+  
+	#cur = db.cursor()
+	#cur.execute("UPDATE CNGL_User_Tbl SET Password = '" + newPasswordHash + "' WHERE UserID = " + userId)
 
 def removePendingUser(userId):
 	userInfo = pendingUserDatabase.getUserInfo(userId)
 	pendingUserDatabase.removeUser(userId)
 	emailDatabase.removeEmail(userInfo['email'])
 	usernameDatabase.removeUsername(userInfo['username'])
+	
+	#db=MySQLdb.connect(host="localhost",user="root",passwd="woot",db="TestDB")
+  
+	#cur = db.cursor()
+	#cur.execute("DELETE FROM CNGL_User_Tbl WHERE UserID = " + userId)
 
 def getUsernameUserId(username):
 	return usernameDatabase.getUsernameUserId(username)
+	
+	#db=MySQLdb.connect(host="localhost",user="root",passwd="woot",db="TestDB")
+  
+	#cur = db.cursor()
+	#cur.execute("SELECT UserID FROM CNGL_User_Tbl WHERE UserName LIKE '" + username + "'")
+	#userName = cur.fetchone()
+	#return userName
 
 def getUserInfo(userId):
 	userStuff = userDatabase.getUserInfo(userId)
@@ -140,6 +178,13 @@ def getNewId():
 
 def getProjectCount():
 	return globalDatabase.getProjectCount()
+	
+	#db=MySQLdb.connect(host="localhost",user="root",passwd="woot",db="TestDB")
+  
+	#cur = db.cursor()
+	#cur.execute("SELECT COUNT(*) FROM CNGL_User_Tbl")
+	#ProjectCount = cur.fetchone()
+	#return ProjectCount
 
 #TODO: make this return status with the userId
 #return 0 if success, -1 if username already exists, -2 if email already exists
@@ -156,6 +201,11 @@ def addPendingUser(email, username, passwordHash):
 	usernameDatabase.addUsername(username, userId)
 	pendingUserDatabase.addUser(userId, email, username, passwordHash)
 	return 0
+	
+	#db=MySQLdb.connect(host="localhost",user="root",passwd="woot",db="TestDB")
+  
+	#cur = db.cursor()
+	#cur.execute("INSERT INTO CNGL_User_Tbl (UserName,Password,EmailAddress) VALUES ('" + username + "','" + passwordHash + "','" + email + "')")
 
 def addKeywordsToProject(projectId, keywordList):
 	keywordDatabase.addKeywordsToProject(projectId, keywordList)
@@ -209,21 +259,55 @@ def getAllPendingUsers():
 
 def getApiKeyUserId(apiKey):
 	return apiKeyDatabase.getApiKeyUserId(apiKey)
+	
+	#db=MySQLdb.connect(host="localhost",user="root",passwd="woot",db="TestDB")
+  
+	#cur = db.cursor()
+	#cur.execute("SELECT UserID FROM CNGL_User_Tbl WHERE APIKey LIKE '" + apiKey + "'")
+	#userID = cur.fetchone()
+	#return userID
+
 
 def addApiKey( apiKey, userId ):
 	apiKeyDatabase.addApiKey(apiKey, userId)
+	
+	#db=MySQLdb.connect(host="localhost",user="root",passwd="woot",db="TestDB")
+  
+	#cur = db.cursor()
+	#cur.execute("UPDATE CNGL_User_Tbl SET APIKey = '" + apiKey + "' WHERE userId = " + userId)
 
 def removeApiKey( apiKey ):
 	apiKeyDatabase.removeApiKey(apiKey)
+	
+	#db=MySQLdb.connect(host="localhost",user="root",passwd="woot",db="TestDB")
+  
+	#cur = db.cursor()
+	#cur.execute("UPDATE CNGL_User_Tbl SET APIKey = '' WHERE APIKey LIKE '" + apiKey + "'")
 
 def getTitleProjectId(title):
 	return titleDatabase.getTitleProjectId(title)
+	
+	#db=MySQLdb.connect(host="localhost",user="root",passwd="woot",db="TestDB")
+  
+	#cur = db.cursor()
+	#cur.execute("SELECT WebserviceID FROM CNGL_Webservices_Tbl WHERE Name LIKE '" + title + "'")
+	#projectID = cur.fetchone()
+	#return projectID
 
 def addTitle( title, projectId ):
 	titleDatabase.addTitle(title, userId)
+	#db=MySQLdb.connect(host="localhost",user="root",passwd="woot",db="TestDB")
+  
+	#cur = db.cursor()
+	#cur.execute("UPDATE CNGL_Webservices_Tbl SET Name = '" + title + "' WHERE WebserviceID = " + projectId)
 
 def removeTitle( title ):
 	titleDatabase.removeTitle(title)
+	
+	#db=MySQLdb.connect(host="localhost",user="root",passwd="woot",db="TestDB")
+  
+	#cur = db.cursor()
+	#cur.execute("UPDATE CNGL_Webservices_Tbl SET Name = '' WHERE WebserviceID = " + projectId)
 
 
 def addUserHit(userId, projectId):
